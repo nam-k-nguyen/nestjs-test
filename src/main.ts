9637375ef18fb1ba3696b0df132b8a924c7cb6dc
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-17 23:11:02 -0400

    Update main.ts
    Change cors configuration

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:3000/react-axios',
-      'http://localhost:3001',
-      'http://localhost:3002',
-      'http://192.168.1.41:3002'
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  });
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors();
 
   await app.listen(process.env.PORT || 3000);
 }
