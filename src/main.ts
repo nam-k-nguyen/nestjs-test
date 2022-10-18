d7ed6c07548e288b48ee35dda9f7f7070d2950cf
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-17 22:21:35 -0400

    Update main.ts
    Add cors and different port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'http://localhost:3002',
+      'http://192.168.1.41:3002'
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  });
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
